a963d47e1f106abb76e706645e7d850f0b60a2fa
thanglkm <thanglkm@vnpt.vn>
2022-06-09 00:54:37 +0700

    @

--- src/app/components/structure-directive/structure-directive.component.ts
+++ src/app/components/structure-directive/structure-directive.component.ts
@@ -9,6 +9,10 @@ export class StructureDirectiveComponent implements OnInit {
 
   text1: any;
   text2: any;
+  text3: any;
+  text4: any;
+  text5: any;
+  text6: any;
 
 
   user: any = {
@@ -25,6 +29,26 @@ export class StructureDirectiveComponent implements OnInit {
     { value: 15 },
     { value: 16 },
     { value: 17 },
+  ];
+
+  employees = [
+    {
+      id: 1,
+      firstName: "Flora",
+      lastName: "Twell",
+      email: "ftwell0@phoca.cz",
+      gender: "Female",
+      ipAddress: "99.180.237.33",
+    },
+    {
+      id: 2,
+      firstName: "Priscella",
+      lastName: "Signe",
+      email: "psigne1@berkeley.edu",
+      gender: "Female",
+      ipAddress: "183.243.228.65",
+    },
+    // more data
   ];
 
   constructor() { }
@@ -55,6 +79,44 @@ export class StructureDirectiveComponent implements OnInit {
     </div>
   </ng-template>
     `;
+    this.text3 = `
+    employees = [
+      {
+        id: 1,
+        name: "Flora",
+        lastName: "Twell",
+        email: "ftwell0@phoca.cz",
+        gender: "Female",
+        ipAddress: "99.180.237.33",
+      },
+      {
+        id: 2,
+        firstName: "Priscella",
+        lastName: "Signe",
+        email: "psigne1@berkeley.edu",
+        gender: "Female",
+        ipAddress: "183.243.228.65",
+      },
+      // more data
+      `;
+    this.text4 = `
+    <div *ngFor="let e of employees">
+    {{e.id}} - {{e.firstName}} {{e.lastName}}
+    </div>
+      `;
+    this.text5 = `
+  <div *ngFor="let employee of employees; index as idx; count as total">
+  ({{idx}})/({{total}}): {{employee.id}} - {{employee.firstName}}
+  {{employee.lastName}}
+  </div>
+      `;
+    this.text6 = `
+  <div *ngFor="”let" item of list”>
+    <div *ngIf="”somethingGoood”">
+      More code
+    </div>
+  </div>
+      `;
   }
 
   changeAge(event: any) {
